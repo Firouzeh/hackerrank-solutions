cf4f702af4899ea1cf55efa3c21f1cbb73275d29
Firouzeh <firouzeh.hejazi@quant.network>
2023-11-12 12:22:28 +0000

    initial commit

---
+++ src/main/java/com/hackerrank/hackerranksolutions/HackerrankSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.hackerrank.hackerranksolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerrankSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackerrankSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackerrank/hackerranksolutions/HackerrankSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerrank.hackerranksolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerrankSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
